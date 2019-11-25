7a396669115177a5879b5bf3807fbb7234d2d5c8
goodtocode <robert.good@goodtocode.com>
2019-11-25 15:05:05 -0800

    Sprint 11 - Removing unnecessary files from support folders

--- Build/Build.Content/Utility/SNK/SNKCreate-RunManually.bat
+++
@@ -1,9 +0,0 @@
-ECHO OFF
-REM Usage: Call SNKCreate.bat GoodToCodeFramework.snk
-ECHO Starting SNKCreate.bat GoodToCodeFramework.snk
-
-%WINDIR%\system32\attrib.exe *.* -r /s
-
-..\Utility\SNK\sn.exe –k GoodToCodeFramework.snk
-
-exit 0
