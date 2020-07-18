8e7ed6c8c363b80670c945829f33261c215d350b
Robert Good <robert.good@goodtocode.com>
2020-07-18 16:19:26 -0700

    Added test factories

---
+++ Src/Extensions.Tests/TestFactory/AppSettingsFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace GoodToCode.Extensions.Test
+{
+    public class AppSettingFactory
+    {
+        private IConfiguration _config;
+
+        public string DefaultConnection { get { return _config["AppSettings:MyWebService"]; } }
+        
+        public AppSettingFactory()
+        {
+            _config = new ConfigurationBuilder()
+              .AddJsonFile($"appsettings.{(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT ") ?? "Development")}.json")
+              .AddJsonFile("appsettings.json")
+              .Build();
+        }
+
+        public AppSettingFactory(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GetDefaultConnection()
+        {
+            return DefaultConnection;
+        }
+    }
+}
